4906d0cae54c04cf517a69aedb9f3aaf8fdf4ce0
Debasish Bhola <devop69@gmail.com>
2024-08-27 01:05:30 +0530

    Changes

--- 4.GROUP BY and ORDER BY.sql
+++ 4.GROUP BY and ORDER BY.sql
@@ -1,7 +1,10 @@
 #GROUP BY
 
-select gender from employee_demographics group by gender;
+select salary,AVG(salary) from employee_salary;
+#Error Code: 1140. In aggregated query without GROUP BY, expression #1 of SELECT list contains nonaggregated column 'parks_and_recreation.employee_salary.salary'; this is incompatible with sql_mode=only_full_group_by
 
+
+select gender from employee_demographics group by gender;
 #select first_name from employee_demographics group by gender; This gives error because we are not using agggregated functions(Like AVG) with first_name
 
 select gender,AVG(age),MAX(age),MIN(age),COUNT(age) from employee_demographics group by gender;
